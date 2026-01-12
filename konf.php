9c7710765214ad72bca01acff182b30621e274ef
Nikita Orlenko <nikita.orlenko234@gmail.com>
2026-01-12 13:05:55 +0200

    Lõin uus fail ja kirjutasin kood et kasutaja saab logi sisse

--- konf.php
+++ konf.php
@@ -3,6 +3,6 @@ $servernimi = "localhost";
 $kasutajanimi = "orlenkoJalgrattas";
 $parool = '1234';
 $andmebaadnimi = 'jalgrattaeksam';
-$connection = new mysqli($servernimi, $kasutajanimi, $parool, $andmebaadnimi);
-$connection->set_charset("utf8")
+$yhendus = new mysqli($servernimi, $kasutajanimi, $parool, $andmebaadnimi);
+$yhendus->set_charset("utf8")
 ?>
